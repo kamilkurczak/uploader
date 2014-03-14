36989544a7a043d5bceac9ff39a55ea4891fee8c
Kamil Kurczak <k.kurczak@erunner.eu>
2014-03-14 13:46:08 +0100

    block possibility to initiate FileType class

--- src/Tokk/Uploader/File/FileType.php
+++ src/Tokk/Uploader/File/FileType.php
@@ -6,4 +6,10 @@ class FileType
 {
     const IMAGE = 'IMAGE';
     const FILE = 'FILE';
+
+    private function __construct() {}
+
+    private function __clone() {}
+
+    private function __wakeup() {}
 }
